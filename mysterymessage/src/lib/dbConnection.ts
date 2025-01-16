09f7dd6ed019e2516a9a5223acc7ad2f1643a153
Bhaven <chaudharybhaven@gmail.com>
2025-01-16 19:30:07 +0530

    Creating  dbConnect function to connect to mongoose database

---
+++ mysterymessage/src/lib/dbConnection.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { deflate } from "zlib";
+
+// Creating type for connection object to add type safety for connection response received from database
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+
+    // check if connection already present 
+    if(connection.isConnected){
+        console.log("Already connected to database")
+        return
+    }
+
+    //Setting up connection to database
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+         connection.isConnected = db.connections[0].readyState; 
+        
+        console.log("Database connected successfully")
+    } catch (error) {
+        console.log("Connecting to database failed", error)
+        process.exit(1)
+    }
+}
+
+export default dbConnect;
